defde47c0bc33261295bcf3baeb8c1d769a44363
Christian Nikolov <christian.nikolov@outlook.com>
2021-05-08 23:01:29 +0100

    Supplier should have many products.

--- InventoryApp.API/InventoryApp.Domain/Supplier.cs
+++ InventoryApp.API/InventoryApp.Domain/Supplier.cs
@@ -1,8 +1,11 @@
-﻿namespace InventoryApp.Domain
+﻿using System.Collections.Generic;
+
+namespace InventoryApp.Domain
 {
     public class Supplier
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public List<Product> Products { get; set; }
     }
 }
